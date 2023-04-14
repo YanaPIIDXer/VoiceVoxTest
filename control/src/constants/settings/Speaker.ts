7d4eb1533e123f01ad88c2307eac596c97b8f538
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2023-04-14 19:57:42 +0900

    定数に移動

---
+++ control/src/constants/settings/Speaker.ts
@@ -0,0 +1,8 @@
+import type { Speaker } from "@/interfaces/settings/Speaker";
+
+/**
+ * 発現させるキャラクタ
+ */
+export const speakers: Speaker[] = [
+  { id: 1, name: "ずんだもん" },
+];
--- control/src/interfaces/settings/Speaker.ts
+++ control/src/interfaces/settings/Speaker.ts
@@ -5,10 +5,3 @@ export interface Speaker {
   id: number;
   name: string;
 };
-
-/**
- * 発現させるキャラクタ
- */
-export const speakers: Speaker[] = [
-  { id: 1, name: "ずんだもん" },
-];
